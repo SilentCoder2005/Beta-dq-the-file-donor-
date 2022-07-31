03d47dce30975c77637a1ae39d125ca62605b01c
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-07-31 21:50:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -517,7 +517,7 @@ async def settings(client, message):
                     callback_data=f'setgs#auto_delete#{settings["auto_delete"]}#{grp_id}',
                 ),
                 InlineKeyboardButton(
-                    '10 Mins' if settings["auto_delete"] '30 Mins' if settings["thirty_mins"] else 'OFF',
+                    '10 Mins' if settings["auto_delete"] else 'OFF',
                     callback_data=f'setgs#auto_delete#{settings["auto_delete"]}#{grp_id}',
                 ),
             ],
